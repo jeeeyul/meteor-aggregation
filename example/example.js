5bfa9d02c1adb6de409332a9ee82fd709f102633
Jeeeyul <Jeeeyul@gmail.com>
2013-12-13 17:31:33 +0900

    dependency

--- example/example.js
+++ example/example.js
@@ -1,19 +1,58 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to example.";
-  };
+People = new Meteor.Collection("people"); 
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+if (Meteor.isClient) {
+	Template.hello.stat = function(){
+		return Session.get("stat");
+	};
+	
+	Template.hello.events({
+		"click #test-aggregation-button" : function(){
+			var pipe = [];
+			pipe.push({
+				$group : {
+					_id : "$favoriteColor",
+					count : {
+						$sum : 1
+					}
+				}
+			});
+			
+			People.aggregate(pipe, function(err, docs){
+				if(docs){
+					Session.set("stat", docs);
+				}
+			});
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function() {
+		if(People.find().count() == 0){
+			People.insert({
+				"name" : "jeeeyul",
+				"favoriteColor" : "red"
+			});
+			People.insert({
+				"name" : "ameluna",
+				"favoriteColor" : "pink"
+			});
+			People.insert({
+				"name" : "kim jeongil",
+				"favoriteColor" : "red"
+			});
+			People.insert({
+				"name" : "someone",
+				"favoriteColor" : "black"
+			});
+			People.insert({
+				"name" : "other",
+				"favoriteColor" : "red"
+			});
+			People.insert({
+				"name" : "pink mania",
+				"favoriteColor" : "pink"
+			});
+		}
+	});
 }
