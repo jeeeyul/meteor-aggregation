a0d6719ed5e8509ac014d543c0f5f8bac077124a
Jeeeyul <Jeeeyul@gmail.com>
2013-12-17 13:04:51 +0900

    exports mongojs db instance as "MongojsDB"

--- package.js
+++ package.js
@@ -9,10 +9,10 @@ Npm.depends({
 
 Package.on_use(function(api, where) {
 	api.use([ "livedata", "mongo-livedata" ], [ "client", "server" ]);
-	
+
 	var path = Npm.require('path');
 	api.add_files(path.join("lib", "server.js"), "server");
 	api.add_files(path.join("lib", "client.js"), "client");
-	
-	api.export("MongojsDB", "server");
+
+	api.export([ "MongojsDB" ], [ "server" ]);
 });
